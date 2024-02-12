aa5c8e6ec1cc9a80413ef646720c1e6838adbd9e
alessandro-giuzio <giuzio@icloud.com>
2024-02-12 20:15:47 +0100

    articles

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const SITE_DESCRIPTION = 'TechPeople Blog';
+export const HOMEPAGE_ARTICLES_LIMIT = 6;
+export const ARTICLES_POR_PAGE = 6;
---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+// Sort articles by date
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export {
+  formatDate,
+}
